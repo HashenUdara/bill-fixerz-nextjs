4c3686b831eea5cd602b947fbbd7ed39e3e42832
Hashen Udara <emhashenudara@gmail.com>
2023-08-16 03:19:56 +0530

    late night update

---
+++ src/components/ServicesData.js
@@ -0,0 +1,48 @@
+import logoSoftware from '@/images/services/9.png'
+import logoRestaurant from '@/images/services/4.png'
+import logoFactory from '@/images/services/6.png'
+import logoAgriculture from '@/images/services/11.png'
+import logoTemperature from '@/images/services/14.png'
+import logoDashboard from '@/images/services/10.png'
+
+const services = [
+  {
+    no: "01",
+    title: "Industrial Software Solutions",
+    description: "Streamline operations and boost efficiency with our tailored software solutions designed to elevate your industrial processes.",
+    logo: logoSoftware,
+  },
+  {
+    no: "02",
+    title: "Temperature Controlling Solutions",
+    description: "Maintain optimal conditions effortlessly using our advanced temperature control solutions, ensuring precision and reliability in every environment.",
+    logo: logoTemperature,
+  },
+  {
+    no: "03",
+    title: "Smart Agriculture Solutions",
+    description: "Cultivate success with our innovative agricultural technologies, empowering you to make informed decisions and maximize yields.",
+    logo: logoAgriculture,
+  },
+  {
+    no: "04",
+    title: "Factory Automation Solutions",
+    description: "Revolutionize your manufacturing processes through cutting-edge automation solutions, enhancing productivity and reducing costs.",
+    logo: logoFactory,
+  },
+  {
+    no: "05",
+    title: "Smart Restaurant Solutions",
+    description: "Redefine the dining experience with our smart restaurant solutions, seamlessly integrating technology to enhance service and customer satisfaction.",
+    logo: logoRestaurant,
+  },
+  {
+    no: "06",
+    title: "Industrial Dashboard Solutions",
+    description: "Gain real-time insights and control over your industrial operations with our customizable dashboard solutions, driving informed decision-making and operational excellence.",
+    logo: logoDashboard,
+  },
+];
+
+
+export default services;
